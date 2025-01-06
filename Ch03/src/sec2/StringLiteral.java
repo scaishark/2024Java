ec3b105991681f762d0d84caabf4a5121741e87a
Sanghyun Park <137844731+scaishark@users.noreply.github.com>
2025-01-06 15:30:44 +0900

    Ch03 added.

---
+++ Ch03/src/sec2/StringLiteral.java
@@ -0,0 +1,12 @@
+package sec2;
+
+public class StringLiteral {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = 3 + 3.0 + "Java";
+		
+		System.out.println(test);
+	}
+
+}
