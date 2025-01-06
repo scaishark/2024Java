b701aabda3778d25222f8f3687e1813eb581bda7
Sanghyun Park <137844731+scaishark@users.noreply.github.com>
2025-01-06 15:31:26 +0900

    Ch05 added.

---
+++ Ch05/src/exam01/Hello.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Hello {
+	String name;
+	
+	public void SayHello() {
+		System.out.println("Hello" + name);
+	}
+}
---
+++ Ch05/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import exam01.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h = new Hello();
+		h.SayHello();		
+	}
+
+}
